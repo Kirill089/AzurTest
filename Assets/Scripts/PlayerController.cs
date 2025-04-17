f08453cc5645c67b1fb4d41b66a38d4c30dd6586
KirillNovgorodov <novgorodov.k089@gmail.com>
2025-04-18 00:00:15 +0400

    Character movement adjustment

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,15 +3,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private CharacterController characterController;
-    [SerializeField] private float speed = 2f;
-    [SerializeField] Animator animator;
-    [SerializeField] private Transform camera;
+    [SerializeField] private float forwardSpeed = 3f;
+    [SerializeField] private float sideOrBackSpeed = 1.5f;
+    [SerializeField] private Animator animator;
+    [SerializeField] private Transform cameraTransform;
 
     private float vertical;
     private float horizontal;
-    private float speedMultiplier = 1f;
-    private float speedClamp = 0f;
-
     private Vector3 moveDirection;
 
     void Update()
@@ -21,47 +19,32 @@ public class PlayerController : MonoBehaviour
 
         moveDirection = new Vector3(horizontal, 0f, vertical).normalized;
 
-        bool isSprinting = Input.GetKey(KeyCode.LeftShift);
-
-        if (vertical > 0)
-        {
-            speedClamp = 2f;
-        }
-        else if (horizontal != 0)
-        {
-            speedClamp = 1.5f;
-        }
-        else
-        {
-            speedClamp = 1f;
-        }
-
-        if (isSprinting)
-        {
-            if (speedMultiplier < speedClamp)
-                speedMultiplier += 0.02f;
-        }
-        else
-        {
-            if (speedMultiplier > 1f)
-                speedMultiplier -= 0.02f;
-        }
-
-        animator.SetFloat("Vertical", vertical * speedMultiplier);
-        animator.SetFloat("Horizontal", horizontal * speedMultiplier);
+        animator.SetFloat("Vertical", vertical);
+        animator.SetFloat("Horizontal", horizontal);
+        animator.SetBool("IsRunning", moveDirection.magnitude > 0.1f);
 
         if (moveDirection.magnitude > 0.1f)
         {
-            float targetAngle = Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg + camera.eulerAngles.y;
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            characterController.Move(moveDir * speed * speedMultiplier * Time.deltaTime);
-
-            if (vertical > 0.1f)
-            {
-                Quaternion targetRotation = Quaternion.Euler(0f, targetAngle, 0f);
-                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 10f);
-            }
+            MoveCharacter();
         }
+    }
 
+    void MoveCharacter()
+    {
+        float currentY = transform.position.y;
+
+        float targetAngle = Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg + cameraTransform.eulerAngles.y;
+        Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+
+        float currentSpeed = vertical > 0.1f ? forwardSpeed : sideOrBackSpeed;
+
+        characterController.Move(moveDir * currentSpeed * Time.deltaTime);
+        transform.position = new Vector3(transform.position.x, currentY, transform.position.z);
+
+        if (vertical > 0.1f)
+        {
+            Quaternion targetRotation = Quaternion.Euler(0f, targetAngle, 0f);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 10f);
+        }
     }
 }
